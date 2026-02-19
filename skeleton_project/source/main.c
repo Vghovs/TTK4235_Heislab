a263576a98c4f952fee1479c536c4e9b6c3fa419
Vghovs <vghovstad@gmail.com>
2026-02-19 15:09:08 +0100

    comment

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -161,7 +161,7 @@ int main(){
 //         }
         
 //         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
-//          ghp_8H1TzdRQSYkDqsM5tF8o7eU0cPb4lw3IVg6
+//          this is not a seacretghp_8H1TzdRQSYkDqsM5tF8o7eU0cPb4lw3IVg
 //     }
 
 //     return 0;
