3c958cb1c531752444ecc9d884c3990a33e25c78
Vghovs <vghovstad@gmail.com>
2026-02-26 15:58:47 +0100

    bugfix

--- skeleton_project/source/functions.h
+++ skeleton_project/source/functions.h
@@ -6,11 +6,11 @@
 void elevatorDown(void);
 void elevatorUp(void);
 void stop(void);
-void performEmergencyStop(bool orderList); 
-void addOrder(int floor, bool orderList);
-void removeOrder(int floor, bool orderList);
-void clearOrders(bool orderList);
+void performEmergencyStop(bool *orderList); 
+void addOrder(int floor, bool *orderList);
+void removeOrder(int floor, bool *orderList);
+void clearOrders(bool *orderList);
 void openDoor(void);
 void closeDoor(void);
 void initializeElevator(void);
-void lookForOrders(bool orderList);
+void lookForOrders(bool *orderList);
